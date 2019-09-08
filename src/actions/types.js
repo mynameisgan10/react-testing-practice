5bb4e8895d19c8d066a05424572ece5c28326d7d
gandeveloper <geekwithgun@gmail.com>
2019-09-08 13:28:33 +0700

    posts reducer with tests

---
+++ src/actions/types.js
@@ -0,0 +1,5 @@
+
+
+export const types = {
+    GET_POSTS: 'getPosts'
+};
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,9 +1,9 @@
 import {combineReducers} from "redux";
-import success from './successReducer'
+import posts from "./posts/reducer"
 
 
 export default combineReducers({
-    success
+    posts
 })
 
 
