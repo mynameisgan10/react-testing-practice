90594470cb53f0dd87e91de3b23e0fea064ad24b
gandeveloper <geekwithgun@gmail.com>
2019-09-10 13:20:56 +0700

    testing methods

--- src/App.js
+++ src/App.js
@@ -17,18 +17,37 @@ const tempArr = [
   }
 ];
 
+const initialState = {
+  hideBtn: false
+};
+
 class App extends Component {
   constructor(props) {
     super(props);
+    this.state = {
+      ...initialState
+    };
     this.fetch = this.fetch.bind(this);
   }
   fetch() {
     this.props.fetchPosts();
+    this.exampleMethod_updatesState()
+  }
+
+  exampleMethod_updatesState() {
+    const { hideBtn } = this.state;
+    this.setState({
+      hideBtn: !hideBtn
+    });
+  }
+
+  exampleMethod_returnsAValue(number) {
+    return number + 1
   }
 
   render() {
     const { posts } = this.props;
-
+    const {hideBtn} = this.state
     const configButton = {
       buttonText: "Get posts",
       emitEvent: this.fetch
@@ -43,17 +62,20 @@ class App extends Component {
             desc="Click the button to render posts"
             tempArr={tempArr}
           />
-          <SharedButton {...configButton} />
-          {posts.length > 0 && <div>{posts.map((post,index) => {
-            const {title,body} = post;
-            const configListItem = {
-              title,
-              desc: body
-            }
-            return (
-              <ListItem key={index} {...configListItem}/>
-            )
-          })}</div>}
+          {!hideBtn && <SharedButton {...configButton} />}
+
+          {posts.length > 0 && (
+            <div>
+              {posts.map((post, index) => {
+                const { title, body } = post;
+                const configListItem = {
+                  title,
+                  desc: body
+                };
+                return <ListItem key={index} {...configListItem} />;
+              })}
+            </div>
+          )}
         </section>
       </div>
     );
--- src/App.test.js
+++ src/App.test.js
@@ -5,8 +5,10 @@ import React from "react";
 
 const setUp = (initialState = {}) => {
   const store = testStore(initialState);
-  const wrapper = shallow(<App store={store} />).childAt(0).dive();
-//   console.log(wrapper.debug())
+  const wrapper = shallow(<App store={store} />)
+    .childAt(0)
+    .dive();
+  //   console.log(wrapper.debug())
   return wrapper;
 };
 
@@ -32,10 +34,22 @@ describe("App Component", () => {
     wrapper = setUp(initialState);
   });
 
+  it("Should render without errors", () => {
+    const component = findByTestAttr(wrapper, "appComponent");
+    expect(component.length).toBe(1);
+  });
 
-  it('Should render without errors', () => {
-      const component = findByTestAttr(wrapper, 'appComponent')
-      expect(component.length).toBe(1)
+  it("exampleMethod_updatesState Method should update state as expected", () => {
+    const classInstance = wrapper.instance();
+    classInstance.exampleMethod_updatesState();
+    const newState = classInstance.state.hideBtn;
+    expect(newState).toBe(true);
+  });
+
+  it("exampleMethod_returnsAValue Method should return value as expected", () => {
+    const classInstance = wrapper.instance();
+    const newValue = classInstance.exampleMethod_returnsAValue(6)
+    expect(newValue).toBe(7)
   })
 
 });
